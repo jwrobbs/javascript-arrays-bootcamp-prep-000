bffaea75d4e3fcda69c5ea68ac8ce3c5d6b7e3bd
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-14 23:57:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(string,string2){
+ var newArray = [string2, ...string]
+ return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(string,string2){
+  return $string.unshift(string2)
+}
+
+function addElementToEndOfArray() {
+  
+}
+function destructivelyAddElementToEndOfArray() {
+  
+}
