6b1b161755069fe161748c5a8780835268fa5b34
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 00:15:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 function removeElementFromEndOfArray(array){
-  newArray = array.slice(-1)
+  newArray = array.slice(0, array.length - 1)
   return newArray
 }
