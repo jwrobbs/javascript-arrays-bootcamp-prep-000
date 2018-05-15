c9d2be3d4c09fe144031e498829c30e1cd2e4f8f
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-15 00:02:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array,element) {
-  var newArray = [...array,string]
+  var newArray = [...array,element]
   return newArray
 }
 function destructivelyAddElementToEndOfArray(array,element) {
